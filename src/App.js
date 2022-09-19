b3530f32eab89dfe6e71f1cd7daf7b383bd3696b
Daria Nebylitsa <daranebylitsa@gmail.com>
2022-09-19 22:26:10 +0300

    first update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+import cat from './assets/cat.gif';
+import moon from './assets/moon.png';
+import land from './assets/land.png';
 
 function App() {
+  const ref = React.useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Parallax pages={4} ref={ref}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer sticky={{ start: 0.4, end: 3.5 }}>
+          <img src={cat} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0.2} speed={0.05} onClick={() => ref.current.scrollTo(3)}>
+          <h2>Welcome to my imagination</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3} speed={2} /* onClick={() => ref.current.scrollTo(0)} */>
+          <h2>Hi there, world</h2>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
